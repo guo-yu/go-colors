858554e46c8eb899648b03e34a32333914fc0bb3
Guo Yu <i@guoyu.me>
2014-07-01 18:14:34 +0800

    add simple example

---
+++ examples/colors.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import ".."
+
+func main() {
+  fmt.Printf("\n")
+  fmt.Printf("Colors list: \n")
+  fmt.Printf("\n")
+  fmt.Printf("  In %s \n", colors.Yellow("Yellow"))
+  fmt.Printf("  In %s \n", colors.Yellow("Yellow"))
+  fmt.Printf("  In %s \n", colors.Yellow("Yellow"))
+  fmt.Printf("\n")
+}
